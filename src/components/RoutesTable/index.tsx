5ed849de4a3557abfd12f93c74dc2e3d2640fa83
Art3mka <87860402+Art3mka@users.noreply.github.com>
2023-06-01 14:55:40 +0300

    update

--- src/components/RoutesTable/index.tsx
+++ src/components/RoutesTable/index.tsx
@@ -14,81 +14,72 @@ import { Table, Button } from 'rsuite'
 const { Column, HeaderCell, Cell } = Table;
 
 
-
-
-
-
-
-
 const RoutesTable = (data: any) => {
+
+    // const [tableInfo, setTableInfo] = useState(data.data)
+
+    // debugger
+    // const sendEmail = (row: any) => {
+    //     console.log('click', row)
+    // }
+
+    // const columns = useMemo<mrtColumnDef<Routes>[]>(
+    //     () => [
+    //         {
+    //             accessorKey: 'routeId',
+    //             header: 'Номер',
+    //             size: 200,
+    //             filterVariant: 'text',
+    //         },
+    //         {
+    //             accessorKey: 'routeName',
+    //             header: 'Маршрут',
+    //             filterVariant: 'text',
+    //             size: 400,
+    //         },
+    //     ],
+    //     []
+    // )
+
+    // const [tableData, setTableData] = useState<Routes[]>(() => data)
+
+    // const handleSaveRow: MaterialReactTableProps<Routes>['onEditingRowSave'] =
+    //     async ({ exitEditingMode, row, values }) => {
+    //         tableData[row.index] = values
+
+    //         setTableData([...tableData])
+    //         exitEditingMode()
+    //     }
+
 
     const [tableInfo, setTableInfo] = useState(data.data)
 
-    debugger
-    const sendEmail = (row: any) => {
-        console.log('click', row)
-    }
-
-    const columns = useMemo<mrtColumnDef<Routes>[]>(
-        () => [
-            {
-                accessorKey: 'routeId',
-                header: 'Номер',
-                size: 200,
-                filterVariant: 'text',
-            },
-            {
-                accessorKey: 'routeName',
-                header: 'Маршрут',
-                filterVariant: 'text',
-                size: 400,
-            },
-        ],
-        []
-    )
-
-    const [tableData, setTableData] = useState<Routes[]>(() => data)
-
-    const handleSaveRow: MaterialReactTableProps<Routes>['onEditingRowSave'] =
-        async ({ exitEditingMode, row, values }) => {
-            tableData[row.index] = values
-
-            setTableData([...tableData])
-            exitEditingMode()
-        }
+    
 
     return (
 
-            <Table
-                className='table__routes'
-                width={1000}
-                height={400}
-                data={data.data || []}
-                onRowClick={rowData => {
-                    console.log(rowData);
-                }}>
+        <Table
+            autoHeight={true}
+            className='table__routes'
+            width={1000}
+            data={data.data || []}
+            onRowClick={rowData => {
+                console.log(rowData);
+            }}
+            >
 
-                <Column width={100} align="center" fixed>
-                    <HeaderCell>Id</HeaderCell>
-                    <Cell dataKey="routeId" />
-                </Column>
+            <Column width={100} align="center" fixed>
+                <HeaderCell>Id</HeaderCell>
+                <Cell dataKey="routeId" />
+            </Column>
 
-                <Column width={500} align="center" fixed>
-                    <HeaderCell>Маршрут</HeaderCell>
-                    <Cell dataKey="routeName" />
-                </Column>
+            <Column width={500} align="center" fixed>
+                <HeaderCell>Маршрут</HeaderCell>
+                <Cell dataKey="routeName" />
+            </Column>
 
-                <Column width={80} fixed="right">
-                    <HeaderCell>...</HeaderCell>
-                    <Cell >
-                        {rowData => (
-                            <Button className='button__edit' appearance="link" onClick={() => alert(`id:${rowData.routeId}`)}>
-                                Edit
-                            </Button>
-                        )}
-                    </Cell>
-                </Column>
-            </Table>
+           
+        </Table>
 
     )
 }
